f07280adc8a5ac678f8884560be50dc32887e37b
Kristin Skipper <kskipper.03@gmail.com>
2020-07-04 17:11:19 -0500

    added more practice

---
+++ src/PalindromPractice.java
@@ -0,0 +1,28 @@
+public class PalindromPractice {
+    
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if(str.charAt(left++) != str.charAt(right--)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "racecar";
+
+        String str2 = "orange";
+        String str3 = "deed";
+        String str4 = "ratemeter";
+
+        System.out.println(isPalindrome(str));
+        System.out.println(isPalindrome(str2));
+        System.out.println(isPalindrome(str3));
+        System.out.println(isPalindrome(str4));
+
+    }
+}
