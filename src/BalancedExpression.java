21cf6bba5c4b1ffc5d85bd203f24d0247cdef3d7
Kristin Skipper <kskipper.03@gmail.com>
2020-07-05 09:31:14 -0500

    added balancedExpression algo problem

--- src/BalancedExpression.java
+++ src/BalancedExpression.java
@@ -1,27 +1,50 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
+
 public class BalancedExpression {
+    private static List<Character> leftBrackets = Arrays.asList('(', '<', '[', '{');
+    private static List<Character> rightBrackets = Arrays.asList(')', '>', ']', ']');
+    
     public static boolean isbalancedExpression(String s) {
         Stack<Character> stack = new Stack<>();
         
         for (char c : s.toCharArray()) {
-            if(c == '(') 
+            if(isLeftBracket(c))
                 stack.push(c);
 
-            if(c == ')'){
+            if (isRightBracket(c)){
                 if(stack.empty()) return false;
-                stack.pop();
+
+                var top = stack.pop();
+                if(bracketsMatch(top, c)) return false;
             }
         }
     
         return stack.empty();
     }
 
-    public static void main(String[] args) {
-        String st = "((<1] + <2>))[a]";
-        String str = "((1 + <2>))[a]";
-        String a = "(1 + 2)";
+    private static boolean isLeftBracket(char c) {
+        return leftBrackets.contains(c);
 
+    }
+
+    private static boolean isRightBracket(char c) {
+        return rightBrackets.contains(c);
+    }
+
+    private static boolean bracketsMatch(char left, char right) {
+        return leftBrackets.indexOf(left) == rightBrackets.indexOf(right);
+    }
+
+    public static void main(String[] args) {
+        String st = "((>1] + <2>))[a]";
+        String str = "((1 + <2>))[a]";
+        String a = "(1 + 2>";
+        var result = isbalancedExpression(st);
+
+        System.out.println(result);
         System.out.println(isbalancedExpression(st));
         System.out.println(isbalancedExpression(str));
         System.out.println(isbalancedExpression(a));
