2c05b30a5163ec80865cc2833fb17bdfe8e54967
Kristin Skipper <kskipper.03@gmail.com>
2020-07-12 17:25:50 -0500

    added more algo practice

---
+++ src/CountAndSay.java
@@ -0,0 +1,27 @@
+public class CountAndSay {
+    public static String countAndSaySequence(int n) {
+        if(n <= 0) return "";
+        
+        String res = "1";
+        while(n > 1) {
+            StringBuffer curr = new StringBuffer();
+            for (int i = 0; i < res.length(); i++) {
+                int count = 1;
+                while (i + 1 < res.length() && res.charAt(i) == res.charAt(i + 1)) {
+                    count++;
+                    i++;
+                }
+                curr.append(count).append(res.charAt(i));
+            }
+            res = curr.toString();
+            n--;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countAndSaySequence(4));
+
+    }
+}
