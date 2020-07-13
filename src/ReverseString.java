1befa52741431107c1fb45172dd1f8ee25244d55
Kristin Skipper <kskipper.03@gmail.com>
2020-07-13 15:19:39 -0500

    added reverse String algo problem

---
+++ src/ReverseString.java
@@ -0,0 +1,26 @@
+import javax.xml.namespace.QName;
+
+public class ReverseString {
+    
+    public static String reverse(String s) {
+        // String reversed = "";
+        // for(int i = s.length() - 1; i >= 0; i--) 
+        //     reversed += s.charAt(i);
+
+        // return reversed;
+        //BETTER WAY IS TO USE STRINGBUILDER
+        if(s == null) return "";
+        StringBuilder reversed = new StringBuilder();
+        for (int i = s.length() - 1; i >=0; i--) {
+            reversed.append(s.charAt(i));
+        }
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        var res = reverse("BLUE");
+        System.out.println(res);
+        var res2 = reverse(null);
+        System.out.println(res2);
+    }
+}
