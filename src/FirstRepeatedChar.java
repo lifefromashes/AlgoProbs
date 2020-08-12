37188ee87a4b6a07acf982fdbf281bd591ca36c4
Kristin Skipper <kskipper.03@gmail.com>
2020-08-11 20:09:43 -0500

    more algo problems

--- src/FirstRepeatedChar.java
+++ src/FirstRepeatedChar.java
@@ -14,5 +14,16 @@ public class FirstRepeatedChar {
     
     return Character.MIN_VALUE;
     }
+
+    public char findFirstRepeatedChar2(String str) {
+        Set<Character> set = new HashSet();
+        for (var c : str.toCharArray()) {
+            if(!set.contains(c)) return c;
+
+            set.add(c);
+        }
+
+        return Character.MIN_VALUE;
+    }
 }
     
